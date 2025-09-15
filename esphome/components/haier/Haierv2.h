23488777cb6abe8a183b987f42a213afd5254129
pakkie1985 <64869512+pakkie1985@users.noreply.github.com>
2025-09-15 10:57:03 +0200

    Update Haierv2.h

--- esphome/components/haier/Haierv2.h
+++ esphome/components/haier/Haierv2.h
@@ -6,74 +6,77 @@ using namespace esphome;
 
 class Haier : public climate::Climate, public PollingComponent {
  public:
-  // Constructor: update interval
-  Haier() : PollingComponent(10000) {}
+  // Constructor
+  Haier() : PollingComponent(15000) {}  // Polling elke 15 seconden
 
-  // Traits van de climate component
+  // ESPHome Climate traits
   climate::ClimateTraits traits() override {
     auto traits = climate::ClimateTraits();
     traits.set_supports_current_temperature(true);
+    traits.set_supports_action(true);
+    traits.set_supports_modes({climate::MODE_OFF, climate::MODE_COOL, climate::MODE_HEAT, climate::MODE_DRY, climate::MODE_FAN_ONLY, climate::MODE_AUTO});
+    traits.set_supports_fan_modes({climate::FAN_OFF, climate::FAN_LOW, climate::FAN_MEDIUM, climate::FAN_HIGH, climate::FAN_AUTO});
+    traits.set_supports_swing_modes({climate::SWING_OFF, climate::SWING_VERTICAL, climate::SWING_HORIZONTAL, climate::SWING_BOTH});
     traits.set_supports_target_temperature(true);
-    traits.set_supports_fan_mode(true);
-    traits.set_supports_swing_mode(true);
-    traits.set_min_temperature(16);
-    traits.set_max_temperature(30);
-    traits.set_temperature_step(1);
-    traits.add_fan_mode(climate::FAN_OFF);
-    traits.add_fan_mode(climate::FAN_LOW);
-    traits.add_fan_mode(climate::FAN_MEDIUM);
-    traits.add_fan_mode(climate::FAN_HIGH);
-    traits.add_fan_mode(climate::FAN_AUTO);
-    traits.add_fan_mode(climate::FAN_FOCUS);
-    traits.add_fan_mode(climate::FAN_DIFFUSE);
-    traits.add_swing_mode(climate::SWING_OFF);
-    traits.add_swing_mode(climate::SWING_VERTICAL);
-    traits.add_swing_mode(climate::SWING_HORIZONTAL);
-    traits.add_swing_mode(climate::SWING_BOTH);
+    traits.set_visual_min_temperature(16.0f);
+    traits.set_visual_max_temperature(30.0f);
     return traits;
   }
 
-  // Polling loop: update state van AC
+  void setup() override {
+    // Setup code hier
+  }
+
   void update() override {
+    // Poll status van de airco
     parse_status();
   }
 
-  // Functie om status van AC uit te lezen (vul hier je protocol in)
-  void parse_status() {
-    // Voorbeeld: waarden van je AC uitlezen
-    // current_temperature_ = ...;
-    // target_temperature_ = ...;
-    // mode_ = ...;
-    // fan_mode_ = ...;
-    // swing_mode_ = ...;
-    publish_state();
-  }
-
-  // Controle van de climate component
   void control(const climate::ClimateCall &call) override {
+    // Mode
+    if (call.get_mode().has_value()) {
+      auto new_mode = *call.get_mode();
+      mode_ = new_mode;
+    }
+
+    // Fan mode
+    if (call.get_fan_mode().has_value()) {
+      auto new_fan = *call.get_fan_mode();
+      fan_mode_ = new_fan;
+    }
+
+    // Swing mode
+    if (call.get_swing_mode().has_value()) {
+      auto new_swing = *call.get_swing_mode();
+      swing_mode_ = new_swing;
+    }
+
+    // Target temperature
     if (call.get_target_temperature().has_value()) {
       target_temperature_ = *call.get_target_temperature();
     }
 
-    if (call.get_mode().has_value()) {
-      mode_ = *call.get_mode();
-    }
+    publish_state();  // Laat ESPHome weten dat de status is veranderd
+  }
 
-    if (call.get_fan_mode().has_value()) {
-      fan_mode_ = *call.get_fan_mode();
-    }
+  void parse_status() {
+    // Hier moet je de echte data van de airco uitlezen en omzetten
+    // Bijvoorbeeld:
+    // current_temperature_ = lees_sensor_temp();
+    // target_temperature_ = lees_airco_setpoint();
+    // mode_, fan_mode_, swing_mode_ = actuele waarden
 
-    if (call.get_swing_mode().has_value()) {
-      swing_mode_ = *call.get_swing_mode();
-    }
+    // Voor demo:
+    current_temperature_ = 24.0;
+    target_temperature_ = 22.0;
+    mode_ = climate::MODE_COOL;
+    fan_mode_ = climate::FAN_AUTO;
+    swing_mode_ = climate::SWING_BOTH;
 
-    // Hier kan je je code toevoegen om de AC fysiek aan te sturen
-    send_command_to_ac();
     publish_state();
   }
 
- protected:
-  void send_command_to_ac() {
-    // Voeg hier je IR / seriële / wifi commando code toe
-  }
+  // Optioneel sensoren
+  sensor::Sensor *temperature_sensor{nullptr};
+  sensor::Sensor *humidity_sensor{nullptr};
 };
