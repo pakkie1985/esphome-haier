82cde541d869ea10861f62ad1544776d97513842
pakkie1985 <64869512+pakkie1985@users.noreply.github.com>
2025-09-15 10:18:57 +0200

    Update climate.py

--- esphome/components/haier/climate.py
+++ esphome/components/haier/climate.py
@@ -3,16 +3,15 @@ import esphome.config_validation as cv
 from esphome.components import climate
 from esphome.const import CONF_NAME
 
-# Namespace en C++ class
-haier_ns = cg.esphome_ns.namespace('haier_v2')
-HaierV2 = haier_ns.class_('HaierV2', climate.Climate, cg.PollingComponent)
+# Namespace voor je component
+haier_ns = cg.esphome_ns.namespace('haier')
+HaierV2 = haier_ns.class_('Haier')  # hier exact de C++ klasse naam
 
-# Nieuwe manier voor schema (ESPHome 2025.11+)
 CONFIG_SCHEMA = climate.climate_schema(HaierV2).extend({
     cv.Required(CONF_NAME): cv.string,
 })
 
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_NAME])
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_NAME], HaierV2)  # <-- hier de klasse meegeven
     cg.add(var)
-    climate.setup_climate(var, config)
+    await climate.register_climate(var, config)
