05928c8567990b61888ac5dfa4a4a2b1baef1dae
pakkie1985 <64869512+pakkie1985@users.noreply.github.com>
2025-09-15 10:11:10 +0200

    Update climate.py

--- esphome/components/haier/climate.py
+++ esphome/components/haier/climate.py
@@ -1,16 +1,18 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome.const import CONF_ID
+from esphome.const import CONF_NAME
 
-haier_ns = cg.esphome_ns.namespace("haier")
-Haier = haier_ns.class_("Haier", cg.Component, climate.Climate)
+# Namespace en class verwijzing naar je C++ HaierV2
+haier_ns = cg.esphome_ns.namespace('haier_v2')
+HaierV2 = haier_ns.class_('HaierV2', climate.Climate, cg.PollingComponent)
 
+# Config schema voor ESPHome
 CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(Haier),
-})
+    cv.Required(CONF_NAME): cv.string,
+}).extend(climate.CLIMATE_SCHEMA)
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await climate.register_climate(var, config)
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_NAME])
+    cg.add(var)
+    climate.setup_climate(var, config)
