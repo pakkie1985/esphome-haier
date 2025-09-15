0a487dcb03775dc9acf53bf6cd17aa4238cd4ff6
pakkie1985 <64869512+pakkie1985@users.noreply.github.com>
2025-09-15 11:06:30 +0200

    Update Haierv2.h

--- esphome/components/haier/Haierv2.h
+++ esphome/components/haier/Haierv2.h
@@ -2,82 +2,71 @@
 
 #include "esphome.h"
 
-using namespace esphome;
+// Alias voor de climate namespace
+namespace cl = esphome::climate;
 
-class Haier : public climate::Climate, public PollingComponent {
+class Haier : public cl::Climate, public PollingComponent {
  public:
-  // Stel hier je update interval in
   Haier() : PollingComponent(2000) {}
 
-  void setup() override {
-    // Setup code hier
-  }
-
+  void setup() override {}
+  
   void update() override {
     parse_status();
   }
 
-  // Traits van de unit
-  climate::ClimateTraits traits() override {
-    auto traits = climate::ClimateTraits();
+  cl::ClimateTraits traits() override {
+    auto traits = cl::ClimateTraits();
+    
+    // Ondersteunde modi
     traits.set_supported_modes({
-        climate::CLIMATE_MODE_OFF,
-        climate::CLIMATE_MODE_COOL,
-        climate::CLIMATE_MODE_HEAT,
-        climate::CLIMATE_MODE_DRY,
-        climate::CLIMATE_MODE_FAN_ONLY,
-        climate::CLIMATE_MODE_HEAT_COOL,
-        climate::CLIMATE_MODE_AUTO
+        cl::CLIMATE_MODE_OFF,
+        cl::CLIMATE_MODE_COOL,
+        cl::CLIMATE_MODE_HEAT,
+        cl::CLIMATE_MODE_DRY,
+        cl::CLIMATE_MODE_FAN_ONLY,
+        cl::CLIMATE_MODE_HEAT_COOL,
+        cl::CLIMATE_MODE_AUTO
     });
+
+    // Ondersteunde fan modes
     traits.set_supported_fan_modes({
-        climate::CLIMATE_FAN_OFF,
-        climate::CLIMATE_FAN_LOW,
-        climate::CLIMATE_FAN_MEDIUM,
-        climate::CLIMATE_FAN_HIGH,
-        climate::CLIMATE_FAN_AUTO
+        cl::CLIMATE_FAN_OFF,
+        cl::CLIMATE_FAN_LOW,
+        cl::CLIMATE_FAN_MEDIUM,
+        cl::CLIMATE_FAN_HIGH,
+        cl::CLIMATE_FAN_AUTO
     });
-    traits.set_supports_current_temperature(true);
+
+    // Swing modes
     traits.set_supports_swing_mode(true);
+
+    // Huidige temperatuur
+    traits.set_supports_current_temperature(true);
+
     return traits;
   }
 
-  void control(const climate::ClimateCall &call) override {
-    if (call.get_mode().has_value()) {
-      auto new_mode = *call.get_mode();
-      mode = new_mode;
-    }
-
-    if (call.get_target_temperature().has_value()) {
-      target_temperature = *call.get_target_temperature();
-    }
-
-    if (call.get_fan_mode().has_value()) {
-      auto new_fan = *call.get_fan_mode();
-      fan_mode = new_fan;
-    }
-
-    if (call.get_swing_mode().has_value()) {
-      auto new_swing = *call.get_swing_mode();
-      swing_mode = new_swing;
-    }
-
+  void control(const cl::ClimateCall &call) override {
+    if (call.get_mode().has_value()) mode = *call.get_mode();
+    if (call.get_target_temperature().has_value()) target_temperature = *call.get_target_temperature();
+    if (call.get_fan_mode().has_value()) fan_mode = *call.get_fan_mode();
+    if (call.get_swing_mode().has_value()) swing_mode = *call.get_swing_mode();
     publish_state();
   }
 
  private:
-  // Hier de interne status van je AC
   float current_temperature{0};
   float target_temperature{24};
-  climate::ClimateMode mode{climate::CLIMATE_MODE_OFF};
-  climate::ClimateFanMode fan_mode{climate::CLIMATE_FAN_AUTO};
-  climate::ClimateSwingMode swing_mode{climate::CLIMATE_SWING_OFF};
+  cl::ClimateMode mode{cl::CLIMATE_MODE_OFF};
+  cl::ClimateFanMode fan_mode{cl::CLIMATE_FAN_AUTO};
+  cl::ClimateSwingMode swing_mode{cl::CLIMATE_SWING_OFF};
 
   void parse_status() {
-    // Hier moet je de status van je AC uitlezen en instellen
-    // Bijvoorbeeld:
-    // current_temperature = sensor_read_temp();
-    // mode = ...; fan_mode = ...; swing_mode = ...;
-
+    // Hier je AC status uitlezen en bovenstaande variabelen bijwerken
+    // voorbeeld:
+    // current_temperature = sensor_value;
+    // target_temperature = ac_setpoint;
     publish_state();
   }
 };
